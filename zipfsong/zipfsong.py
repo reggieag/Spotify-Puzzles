4074128b23e0f4c368554d29791e588c10e06e56
reggieag <reggieag@gmail.com>
2013-03-05 00:25:48 -0500

    2nd commit

--- zipfsong/zipfsong.py
+++ zipfsong/zipfsong.py
@@ -1,2 +1,24 @@
+#zipfsong
+import fileinput
+import re
+
 data=[]
-data.append([line for line in raw_input()])
+
+
+for line in fileinput.input():
+	line = re.split(r'\s', line)
+    data.append(line)
+
+numSongs = int(data[0][0])
+numSelect = int(data[0][1])
+songs = data[1:]
+#songs looks like [['30','one'],['30','two']]
+#Remember we will need the position of the song
+
+def quality(songs):
+	qi=[]
+    for x in songs:
+        
+#possible solution is to normalize according to songs[-1] and then multiply by position
+
+#take songs[-1] and multiply according to position, then divide actualy plays
